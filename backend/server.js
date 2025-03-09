e2723476e86d7eb85a871eeca8e8db99d924122a
Radhakkrishnan9.2@gmail.com <radhakkrishnan9.2@gmail.com>
2025-03-09 17:50:29 +0530

    Removed .env from Git tracking

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,11 @@ dbConnnect()
 app.use(express.json())
 app.use(cors())
 
+app.get("/", (req, res) => {
+    res.send("Backend is running!");
+  });
+  
+
 app.use("/api/auth",authRoutes)
 app.use("/api/user",userRoutes)
 
